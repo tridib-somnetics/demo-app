aa60253c8f764622674c1ab63560ac4c7e6acb4b
Tridib <tridib@ndl.gov.in>
2025-08-05 14:00:32 +0530

    Dockerfile added and config.php modified

--- config.php
+++ config.php
@@ -2,10 +2,11 @@
 mysqli_report(MYSQLI_REPORT_OFF);
 
 # Database credentials
-define("DB_SERVER", "db.tn-homelab.net");
-define("DB_USERNAME", "prod-user");
-define("DB_PASSWORD", "prod4321#");
-define("DB_NAME", "proddb");
+
+define("DB_SERVER", getenv("DB_SERVER"));
+define("DB_USERNAME", getenv("DB_USERNAME"));
+define("DB_PASSWORD", getenv("DB_PASSWORD"));
+define("DB_NAME", getenv("DB_NAME"));
 
 # Create connection
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
